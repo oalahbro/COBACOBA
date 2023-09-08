19ca0293b1f867aacafe50f6fad4c808f433997c
EKAK <epradana15@gmail.com>
2023-09-09 00:41:21 +0700

    butuh fix

--- tt.js
+++ tt.js
@@ -7,14 +7,11 @@ const https = require('https');
 const fs = require('fs');
 const { MessageMedia } = require('whatsapp-web.js');
 const tiktok_url = "https://www.tiktok.com/@ugm.id/video/7273380937675640069"
-
-
-
-
+let bro = 0
 
 const client = new Client({
     authStrategy: new LocalAuth(),
-    puppeteer: { headless: false, executablePath: '/opt/google/chrome/google-chrome' },
+    puppeteer: { headless: false, executablePath: 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe' },
 
 });
 client.initialize();
@@ -33,7 +30,7 @@ client.on('ready', () => {
     console.log('Client is ready!');
 });
 
-var foo = async (msg) => {
+var foo = async () => {
     // do something
    var bar = await TiktokDL(tiktok_url).then((result) => {
         urlnya = JSON.stringify(result)
@@ -54,22 +51,28 @@ var foo = async (msg) => {
             const file = fs.createWriteStream(`${id}.mp4`);
             const request = https.get(link, function(response) {
               response.pipe(file);
-              
             });
-            // const media = MessageMedia.fromUrl(link);
-            let media = MessageMedia.fromFilePath(`./${id}.mp4`)
-            client.sendMessage("6281927442421@c.us", media)
-            msg.reply(media);
+            bro = id;
+            // console.log(id)
         }
 
     })
   }
 
 client.on('message', msg => {
-    if (msg.body == '/ping') {
-        foo(msg)
-        // console.log(foo())
-        //client.sendMessage(nomer + "@c.us", media)
+    if (msg.body == '/lop') {
+        foo()
+        if (bro == 0) {
+            foo()
+            let media = MessageMedia.fromFilePath(bro+'.mp4')
+            console.log(bro+'okeeeee')
+            msg.reply(media);
+        } else {
+            let media = MessageMedia.fromFilePath(bro+'.mp4')
+            console.log(bro+'okeeeee')
+            msg.reply(media);
+        }
+        
         
     }
 });
