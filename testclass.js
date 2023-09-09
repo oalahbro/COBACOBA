0a4b8c827c9bdaaae2591b79ffb95f85e5862243
evantoday <kreasievan@gmail.com>
2023-09-10 00:34:29 +0700

    fyuhh

--- testclass.js
+++ testclass.js
@@ -1,5 +1,78 @@
-const TiktokDownloader = require('./tiktokDownloader'); // Adjust the path to the actual location of tiktokDownloader.js
-
+const { TiktokDL } = require("@tobyg74/tiktok-api-dl")
+const { Client, LocalAuth } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
+const urlRegex = require("url-regex");
+const fetch = require("node-fetch");
+const https = require('https');
+const fs = require('fs');
+const { MessageMedia } = require('whatsapp-web.js');
+const { url } = require("inspector");
+const TiktokDownloader = require('./tiktokDownloader'); 
+// Adjust the path to the actual location of tiktokDownloader.js
 const tiktokDownloader = new TiktokDownloader();
-const tiktok_url = "https://www.tiktok.com/@salzabilll_/video/7275737700462775557";
-tiktokDownloader.downloadTiktokVideo(tiktok_url);
+const client = new Client({
+    authStrategy: new LocalAuth(),
+    puppeteer: { headless: false, executablePath: '/opt/google/chrome/google-chrome' },
+  
+  });
+  client.initialize();
+  client.on('qr', (qr) => {
+    qrcode.generate(qr, {small: true});
+  }); ``
+  client.on('authenticated', () => {
+    console.log('AUTHENTICATED');
+  });
+  client.on('auth_failure', msg => {
+    console.error('AUTHENTICATION FAILURE', msg);
+  });
+  
+  client.on('ready', () => {
+  
+    console.log('Client is ready!');
+  });
+
+
+
+
+  async function dlsend(message) {
+    try {
+      const tiktok_url = "https://www.tiktok.com/@salzabilll_/video/7275737700462775557";
+      const urlParts = tiktok_url.split("/");
+      let id = urlParts[urlParts.length - 1];
+  
+      if (id.includes("?")) {
+        id = id.split("?")[0];
+      }
+  
+      const namefile = id + '.mp4';
+  
+      // Download the TikTok video
+      await tiktokDownloader.downloadTiktokVideo(tiktok_url);
+     // await new Promise(resolve => setTimeout(resolve, 5000));
+  
+      // Create a MessageMedia object from the downloaded video
+      const media = MessageMedia.fromFilePath('./' + namefile);
+  
+      // Reply with the downloaded video
+      await message.reply(media);
+  
+      console.log('Downloaded and sent the TikTok video.');
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  }
+  
+  client.on('message', async message => {
+    let chat = await message.getChat();
+    await chat.sendSeen();
+  
+    if (message.body === '/tt') {
+      await dlsend(message);
+    }
+  });
+  
+  
+  
+  
+  
+  
--- tiktokDownloader.js
+++ tiktokDownloader.js
@@ -7,7 +7,7 @@ const https = require('https');
 const fs = require('fs');
 const { MessageMedia } = require('whatsapp-web.js');
 const { url } = require("inspector");
-// const tiktok_url = "https://www.tiktok.com/@salzabilll_/video/7275737700462775557"
+ const tiktok_url = "https://www.tiktok.com/@salzabilll_/video/7275737700462775557"
 
 
 // const getTiktokUrls = async (tiktok_url) => {
@@ -54,7 +54,6 @@ const { url } = require("inspector");
 
 
 
-
 class TiktokDownloader {
   constructor() {
     // You can initialize any class-level properties here
@@ -78,29 +77,37 @@ class TiktokDownloader {
   }
 
   async downloadTiktokVideo(tiktok_url) {
-    const urlParts = tiktok_url.split("/");
-    let id = urlParts[urlParts.length - 1];
-    if (id.includes("?")) id = id.split("?")[0];
+    return new Promise(async (resolve, reject) => {
+      const urlParts = tiktok_url.split("/");
+      let id = urlParts[urlParts.length - 1];
+      if (id.includes("?")) id = id.split("?")[0];
 
-    const file = fs.createWriteStream(`${id}.mp4`);
-    
-    try {
-      const urls = await this.getTiktokUrls(tiktok_url);
-      if (!urls) {
-        throw new Error("Video URL not found.");
+      const filename = `${id}.mp4`;
+      const file = fs.createWriteStream(filename);
+
+      try {
+        const urls = await this.getTiktokUrls(tiktok_url);
+        if (!urls) {
+          throw new Error("Video URL not found.");
+        }
+
+        const request = https.get(urls, function(response) {
+          response.pipe(file);
+          response.on('end', () => {
+            resolve(filename); // Resolve the promise with the filename when done
+          });
+        });
+        
+        request.on('error', (error) => {
+          console.error(error);
+          reject(error); // Reject the promise if there's an error
+        });
+      } catch (error) {
+        console.error(error);
+        reject(error);
       }
-
-      const request = https.get(urls, function(response) {
-        response.pipe(file);
-      });
-    } catch (error) {
-      console.error(error);
-    }
+    });
   }
 }
-module.exports = TiktokDownloader;
 
-// Usage
-const tiktokDownloader = new TiktokDownloader();
-const tiktok_url = "https://www.tiktok.com/@salzabilll_/video/7275737700462775557";
-tiktokDownloader.downloadTiktokVideo(tiktok_url);
+module.exports = TiktokDownloader;
